0ed432095dcd98f090a0236e6ef658fc98f578d9
Carson McLean <camclean@ualberta.ca>
2016-09-30 23:32:14 -0600

    Squashed commit of the following:
    commit aba6126dfeb991e112e07d6a7802f4c35e1f5e05
    Author: Carson McLean <camclean@ualberta.ca>
    Date:   Fri Sep 30 23:17:28 2016 -0600
    
        Prints HTTP response to stdout with HTTPResponse object
    
    commit f9a90cd466323cfc8610c08950985588373a586f
    Author: Carson McLean <camclean@ualberta.ca>
    Date:   Fri Sep 30 18:19:14 2016 -0600
    
        Refactors GET request string.
    
    commit 14277a2da738342c15115e6b188d5c50cbea4070
    Author: Carson McLean <camclean@ualberta.ca>
    Date:   Fri Sep 30 18:16:38 2016 -0600
    
        Adds POST. All tests passing.
    
    commit 3459a813e63a7a6658bf736332670c6b25020c24
    Author: Carson McLean <camclean@ualberta.ca>
    Date:   Fri Sep 30 17:35:39 2016 -0600
    
        Improved GETs. Tests passing for GETs.
    
    commit a658bcd7dd6856de04a12b57e44c489c28a961da
    Author: Carson McLean <camclean@ualberta.ca>
    Date:   Fri Sep 30 15:07:17 2016 -0600
    
        GET starting to work
    
    commit 1e7c782f0edb553033e325acfa3e254381172660
    Author: Carson McLean <camclean@ualberta.ca>
    Date:   Fri Sep 30 13:05:13 2016 -0600
    
        Added myself to the httpclient.py copyright

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust, Carson McLean
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
+from urlparse import urlparse
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
@@ -31,22 +32,47 @@ class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
+    # def __str__(self):
+    #     return self.body
 
 class HTTPClient(object):
     #def get_host_port(self,url):
 
+    clientSocket = None
+
     def connect(self, host, port):
         # use sockets!
+        if port is None:
+            port = 80
+
+        # Joshua Campbell
+        # CMPUT 404 Lab 2
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientSocket.connect((str(host), int(port)))
+
         return None
 
     def get_code(self, data):
-        return None
+        try:
+            code = int(data.split(" ")[1])
+        except:
+            code = 400
+
+        return code
 
     def get_headers(self,data):
-        return None
+        try:
+            headers = data.split("\r\n\r\n")[0]
+        except:
+            headers = ""
+        return headers
 
     def get_body(self, data):
-        return None
+        try:
+            body = data.split("\r\n\r\n")[1]
+        except:
+            body = ""
+        return body
 
     # read everything from the socket
     def recvall(self, sock):
@@ -61,13 +87,72 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        # TODO: Fix this. Had trouble with Python "if not" concept
+        if  (url.startswith("http://") or url.startswith("https://")):
+            parsed_url = urlparse(url)
+        else:
+             url = "http://" + url
+             parsed_url = urlparse(url)
+
+        self.connect(parsed_url.hostname, parsed_url.port)
+
+        file = parsed_url.path
+        if file=="":
+            file = "/"
+
+        HTTP_request = ("GET " + file + " HTTP/1.1\r\n"
+                        "Host: " + parsed_url.hostname + "\r\n"
+                        "Accept: */*\r\n"
+                        "Connection: Close\r\n\r\n")
+
+        self.clientSocket.send(HTTP_request)
+
+        received = self.recvall(self.clientSocket)
+        print(received)
+
+        code = self.get_code(received)
+        body = self.get_body(received)
+
+        return HTTPResponse(code,body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        # TODO: Fix this. Had trouble with Python "if not" concept
+        if  (url.startswith("http://") or url.startswith("https://")):
+            parsed_url = urlparse(url)
+        else:
+             url = "http://" + url
+             parsed_url = urlparse(url)
+        self.connect(parsed_url.hostname, parsed_url.port)
+
+        file = parsed_url.path
+        if file=="":
+            file = "/"
+
+        if args:
+            encoded_args = urllib.urlencode(args)
+            length = len(encoded_args)
+        else:
+            encoded_args = ""
+            length = 0
+
+        content_type = "application/x-www-form-urlencoded"
+
+        HTTP_request = ("POST " + file + " HTTP/1.1\r\n"
+                        "Host: " + parsed_url.hostname + "\r\n"
+                        "Accept: */*\r\n"
+                        "Content-Length: " + str(length) + "\r\n"
+                        "Content-Type: " + content_type + "\r\n"
+                        "Connection: Close\r\n\r\n")
+        HTTP_request += encoded_args
+
+        self.clientSocket.send(HTTP_request)
+
+        received = self.recvall(self.clientSocket)
+        print(received)
+
+        code = self.get_code(received)
+        body = self.get_body(received)
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -75,7 +160,7 @@ class HTTPClient(object):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
-    
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -85,4 +170,4 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 3):
         print client.command( sys.argv[2], sys.argv[1] )
     else:
-        print client.command( sys.argv[1] )   
+        print client.command( sys.argv[1] )
